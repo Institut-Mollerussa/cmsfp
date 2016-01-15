f30c5896380929840d7eb0a1e648f668a5d83123
Maurici Mòdol Ibàñez <mmodol2@insmollerussa.edu>
2016-01-15 12:16:57 +0100

    correccio dades de configuracio del portal

--- config.php
+++ config.php
@@ -5,8 +5,8 @@
 /*	utilitzant variable global PHP 4.0<
 	global $CFG;
 	
-	$CFG->dirroot="f:\\xampp\\htdocs\\c5\\_portalcss";
-	$CFG->wwwroot="http://localhost/c5/_portalcss/";
+	$CFG->dirroot="f:\\xampp\\htdocs\\m9\\cmsfp";
+	$CFG->wwwroot="http://localhost/m9/cmsfp/";
 	$CFG->dbhost="localhost";
 	$CFG->dbname="portal";
 	$CFG->dbuser="root";
@@ -15,7 +15,7 @@
 
 	class cCFG {
 		public $dirroot="f:\\xampp2\\htdocs\\m9\\cmsfp";
-		public $wwwroot="http://localhost/m8/cmsfp";
+		public $wwwroot="http://localhost/m9/cmsfp";
 		public $dbhost="localhost";
 		public $dbname="portal";
 		public $dbuser="root";
